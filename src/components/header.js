e434e324fddb017d96051cd9fd2be87260eb53bb
Muhamad Fajri <hurifajri@gmail.com>
2020-12-05 13:15:10 +0700

    Create search field component

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ export default () => (
     <div className="col-12">
       <div className="row">
         <div className="col-6">
-          <input type="text" placeholder="Search Furniture" />
+          <SearchField type="text" placeholder="Search Furniture" />
         </div>
       </div>
       <div className="row">
@@ -19,4 +19,18 @@ export default () => (
 
 const Header = styled.header`
   background-color: var(--main-color);
+`;
+
+const SearchField = styled.input`
+  color: var(--white-color);
+  font-size: 1rem;
+  font-weight: 600;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid var(--white-color);
+  width: 100%;
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
 `;
