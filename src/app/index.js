5c40da6900d609458df34596ae1490cf832fe7ee
Muhamad Fajri <hurifajri@gmail.com>
2020-12-05 11:07:47 +0700

    Initial page

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import './simple-grid.min.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => (
+  <Container className="container">
+    <Header className="row">
+      <div className="col-12">
+        <div className="row">
+          <div className="col-12">search bar</div>
+        </div>
+        <div className="row">
+          <div className="col-6">filter 1</div>
+          <div className="col-6">filter 2</div>
+        </div>
+      </div>
+    </Header>
+    <Main className="row">
+      <div className="col-12">
+        <div className="row">
+          <div className="col-6">Product 1</div>
+          <div className="col-6">Product 2</div>
+        </div>
+        <div className="row">
+          <div className="col-6">Product 3</div>
+          <div className="col-6">Product 4</div>
+        </div>
+      </div>
+    </Main>
+  </Container>
+);
 
-export default App;
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const Main = styled.main``;
