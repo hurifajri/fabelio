c0047d6f9168d6e387b7f020672f5740f4f34770
Muhamad Fajri <hurifajri@gmail.com>
2020-12-06 22:09:09 +0700

    Split constant to separated file

---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const BASE = 'http://www.mocky.io/v2';
+export const ID = '/5c9105cb330000112b649af8';
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import * as Path from './constant';
 import GlobalStyle from './global-style';
 
-export { GlobalStyle };
+export { GlobalStyle, Path };
