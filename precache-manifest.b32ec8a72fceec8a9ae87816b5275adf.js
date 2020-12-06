25783347f2639c4ad9738f3145d885cc7872d3fc
Muhamad Fajri <hurifajri@gmail.com>
2020-12-07 01:57:22 +0700

    Updates

---
+++ precache-manifest.b32ec8a72fceec8a9ae87816b5275adf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f28c255bf54e9caa7adc309ee51452",
+    "url": "/fabelio/index.html"
+  },
+  {
+    "revision": "9d4660863d974b7f1e9c",
+    "url": "/fabelio/static/js/2.ca8906c3.chunk.js"
+  },
+  {
+    "revision": "584c45417e82645276fc6d4400fbae22",
+    "url": "/fabelio/static/js/2.ca8906c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2540f924506da42e206",
+    "url": "/fabelio/static/js/main.d1d16d48.chunk.js"
+  },
+  {
+    "revision": "5b77c8cdf11cb31f45b8",
+    "url": "/fabelio/static/js/runtime-main.25b43db3.js"
+  }
+]);
