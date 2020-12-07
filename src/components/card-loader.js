4381570ca3c655736a5d2c279b9aa35eea3bfe47
Muhamad Fajri <hurifajri@gmail.com>
2020-12-07 21:17:58 +0700

    Modify card loader

--- src/components/card-loader.js
+++ src/components/card-loader.js
@@ -12,22 +12,22 @@ export default props => (
     {/* Title */}
     <rect x="50" y="50" rx="3" ry="3" width="250" height="15" />
     {/* Subtitle */}
-    <rect x="480" y="50" rx="3" ry="3" width="150" height="12" />
+    <rect x="500" y="50" rx="3" ry="3" width="150" height="12" />
     {/* Content */}
-    <rect x="50" y="91" rx="3" ry="3" width="530" height="7" />
-    <rect x="50" y="113" rx="3" ry="3" width="525" height="7" />
-    <rect x="50" y="139" rx="3" ry="3" width="520" height="7" />
+    <rect x="50" y="111" rx="3" ry="3" width="530" height="9" />
+    <rect x="50" y="143" rx="3" ry="3" width="525" height="9" />
+    <rect x="50" y="175" rx="3" ry="3" width="520" height="9" />
     {/* List */}
-    <rect x="50" y="175" rx="3" ry="3" width="200" height="12" />
+    <rect x="50" y="220" rx="3" ry="3" width="200" height="12" />
     {/* Footer title */}
-    <rect x="430" y="210" rx="3" ry="3" width="200" height="12" />
+    <rect x="450" y="250" rx="3" ry="3" width="200" height="12" />
     {/* Top line */}
-    <rect x="5" y="8" rx="3" ry="3" width="669" height="7" />
+    <rect x="5" y="8" rx="3" ry="3" width="695" height="7" />
     {/* Right line  */}
-    <rect x="670" y="9" rx="3" ry="3" width="7" height="247" />
+    <rect x="693" y="9" rx="3" ry="3" width="7" height="400" />
     {/* Bottom line */}
-    <rect x="6" y="250" rx="3" ry="3" width="669" height="7" />
+    <rect x="6" y="293" rx="3" ry="3" width="695" height="7" />
     {/* Left line */}
-    <rect x="4" y="8" rx="3" ry="3" width="7" height="247" />
+    <rect x="4" y="8" rx="3" ry="3" width="7" height="400" />
   </CardLoader>
 );
