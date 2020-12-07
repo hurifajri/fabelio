3ebfcff27facb2533636581bb311546cbe83b9e7
Muhamad Fajri <hurifajri@gmail.com>
2020-12-07 09:04:36 +0700

    Update card-loader

--- src/components/card-loader.js
+++ src/components/card-loader.js
@@ -10,19 +10,26 @@ const CardLoader = function (props) {
       foregroundColor="#dbdbdb"
       {...props}
     >
-      <rect x="50" y="50" rx="3" ry="3" width="250" height="10" />
-      <rect x="475" y="50" rx="3" ry="3" width="150" height="15" />
-      <rect x="50" y="91" rx="3" ry="3" width="525" height="6" />
-      <rect x="50" y="113" rx="3" ry="3" width="500" height="7" />
-      <rect x="50" y="139" rx="3" ry="3" width="525" height="6" />
-      <rect x="50" y="162" rx="3" ry="3" width="475" height="7" />
-      <rect x="50" y="189" rx="3" ry="3" width="500" height="6" />
-      <rect x="50" y="225" rx="3" ry="3" width="150" height="10" />
-      <rect x="475" y="250" rx="3" ry="3" width="150" height="10" />
-      <rect x="4" y="8" rx="3" ry="3" width="7" height="288" />
-      <rect x="6" y="289" rx="3" ry="3" width="669" height="8" />
-      <rect x="670" y="9" rx="3" ry="3" width="6" height="285" />
+      {/* Title */}
+      <rect x="50" y="50" rx="3" ry="3" width="250" height="15" />
+      {/* Subtitle */}
+      <rect x="480" y="50" rx="3" ry="3" width="150" height="12" />
+      {/* Content */}
+      <rect x="50" y="91" rx="3" ry="3" width="530" height="7" />
+      <rect x="50" y="113" rx="3" ry="3" width="525" height="7" />
+      <rect x="50" y="139" rx="3" ry="3" width="520" height="7" />
+      {/* List */}
+      <rect x="50" y="175" rx="3" ry="3" width="200" height="12" />
+      {/* Footer title */}
+      <rect x="430" y="210" rx="3" ry="3" width="200" height="12" />
+      {/* Top line */}
       <rect x="5" y="8" rx="3" ry="3" width="669" height="7" />
+      {/* Right line  */}
+      <rect x="670" y="9" rx="3" ry="3" width="7" height="247" />
+      {/* Bottom line */}
+      <rect x="6" y="250" rx="3" ry="3" width="669" height="7" />
+      {/* Left line */}
+      <rect x="4" y="8" rx="3" ry="3" width="7" height="247" />
     </ContentLoader>
   );
 };
